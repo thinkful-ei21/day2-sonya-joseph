7c6590cf88eadf7cc9760b492d3c6c25ea6ce1f1
Sonya Slegers <sonjay08@gmail.com>
2018-05-08 11:34:29 -0500

    min-max

---
+++ min-max.js
@@ -0,0 +1,29 @@
+'use strict';
+
+function min(numbers) {
+  let min = numbers[0];
+  let i = 0;
+  while (i < numbers.length) {
+    if (numbers[i] < min) {
+      min = numbers[i]; 
+    }
+    i++;
+  }
+  return min;
+}
+
+function max(numbers) {
+  let max = numbers[0];
+  let i = 0;
+  while (i < numbers.length) {
+    if (numbers[i] > max) {
+      max = numbers[i];
+    }
+    i++
+  }
+  return max;
+}
+
+const arr = [1, 25, 63, 13, 100];
+console.log(min(arr));
+console.log(max(arr));
